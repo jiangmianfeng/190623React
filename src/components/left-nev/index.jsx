204fed7416b655dafa0123f7adfdedfd379efce8
jiangmianfeng <45776031+jiangmianfeng@users.noreply.github.com>
2019-07-05 09:30:59 +0800

    导航

--- src/components/left-nev/index.jsx
+++ src/components/left-nev/index.jsx
@@ -1,11 +1,145 @@
 import React,{Component} from 'react';
-import "./index.less"
-export default class LeftNev extends Component{
+import "./index.less";
+import logo from'../../assets/images/logo.png';
+import {Link,withRouter} from 'react-router-dom';
+import { Menu, Icon } from 'antd';
+import MenuList from '../../pages/config/menuConfig';
+const { SubMenu } = Menu;
+ class LeftNev extends Component{
+    getMenuNode_map=(MenuList)=>{
+        return MenuList.map(item=>{
+                console.log(item);
+                if(!item.children){
+                    return (
+                        <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </Link>
+                        </Menu.Item>
+                    )
+                }else{
+                    return(
+                        <SubMenu
+                            key={item.key}
+                            title={
+                                <span>
+                                    <Icon type={item.icon} />
+                                    <span>{item.title}</span>
+                                </span>
+                            }
+                        >
+                            {
+                                this.getMenuNode(item.children)
+                            }
+                        </SubMenu>
+                    )
+                }
+            }
+        )
+    };
+    getMenuNode=(MenuList)=>{
+      return MenuList.reduce((pre,item)=>{
+            if(!item.children){
+                pre.push((
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon} />
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                ))
+            }else{
+                pre.push((
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                    <Icon type={item.icon} />
+                                    <span>{item.title}</span>
+                                </span>
+                        }
+                    >
+                        {
+                            this.getMenuNode(item.children)
+                        }
+                    </SubMenu>
+                ))
+            }
+            return pre;
+      },[]);
+    };
     render(){
+        let path=this.props.location.pathname;
+        console.log('render()',path);
         return (
             <div className="left-nev">
-                left-nev
+                <Link to='/' className="left-nev-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>
+                        上海电气设计
+                    </h1>
+                </Link>
+                <Menu
+                    mode="inline"
+                    theme="dark"
+                    selectedKeys={[path]}
+                >
+                    {
+                        this.getMenuNode(MenuList)
+                    }
+                </Menu>
+                {/*<Menu.Item key="home">
+                        <Link to='./home'>
+                            <Icon type="pie-chart" />
+                            <span>首页</span>
+                        </Link>
+                    </Menu.Item>
+                    <SubMenu
+                        key="sub1"
+                        title={
+                            <span>
+                                <Icon type="mail" />
+                                <span>商品</span>
+                            </span>
+                        }
+                    >
+                        <Menu.Item key="category">
+                            <Link to='./category'>
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>分类管理</span>
+                                </span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="product">
+                            <Link to='./product'>
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>产品管理</span>
+                                </span>
+                            </Link>
+                        </Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item key="role">
+                        <Link to='./role'>
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>角色管理</span>
+                                </span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="user">
+                        <Link to='./user'>
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>用户管理</span>
+                                </span>
+                        </Link>
+                    </Menu.Item>*/}
             </div>
         )
     }
 }
+export default withRouter(LeftNev)
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,9 +1,17 @@
 import React,{Component} from 'react';
 import memoryUtils from '../../utils/memoryUtils';
-import {Redirect} from 'react-router-dom';
+import {Redirect,Switch,Route} from 'react-router-dom';
 import { Layout } from 'antd';
 import LeftNev from'../../components/left-nev';
 import Head from '../../components/header'
+import Home from '../home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import Role from '../role/role'
+import User from '../user/user'
+import Bar from '../chart/bar'
+import Line from '../chart/line'
+import Pie from '../chart/pie'
 
 const { Header, Footer, Sider, Content } = Layout;
 export default class Admin extends Component{
@@ -13,18 +21,30 @@ export default class Admin extends Component{
            return <Redirect to='/login'/>
         }
         return (
-            <Layout style={{Height: '100%',backgroundColor:'red'}}>
+            <Layout style={{height: '100%'}}>
                 <Sider>
                     <LeftNev/>
                 </Sider>
                 <Layout>
-                    <Header>
+                    <Header style={{height:80,backgroundColor: '#cccccc'}}>
                         <Head/>
                     </Header>
                     <Content style={{backgroundColor:'#fff'}}>
-                        Content
+                        <Switch>
+                            <Route path='/home' component={Home}/>
+                            <Route path='/category' component={Category}/>
+                            <Route path='/product' component={Product}/>
+                            <Route path='/user' component={User}/>
+                            <Route path='/role' component={Role}/>
+                            <Route path="/chart/bar" component={Bar}/>
+                            <Route path="/chart/pie" component={Pie}/>
+                            <Route path="/chart/line" component={Line}/>
+                            <Redirect from='/' exact to='/home'/>
+                        </Switch>
                     </Content>
-                    <Footer>Footer</Footer>
+                    <Footer style={{color:'#cccccc',textAlign:'center'}}>
+                        推荐使用谷歌浏览器，可以获得更佳页面操作体验
+                    </Footer>
                 </Layout>
             </Layout>
         )
